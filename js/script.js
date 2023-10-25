f1c0daa5a0dcdd14d5bfcabbfef00ee43a5f9094
unknown <wallethan528@gail.com>
2023-10-24 23:13:11 -0400

    js hotspot stuff

--- js/script.js
+++ js/script.js
@@ -11,3 +11,36 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+(() => {
+  //console.log("IIFE Fired");
+  //variables
+  const model = document.querySelector("#model");
+  const hotspots = document.querySelectorAll(".Hotspot");
+
+  //functions
+  function modelLoaded() {
+    //console.log(hotspots);
+    hotspots.forEach(hotspot => {
+      hotspot.style.display = "block";
+    });
+  }
+
+  function showInfo() {
+    let selected = document.querySelector(`button[slot="${this.slot}"] > div`);
+    gsap.to(selected, 1, { autoAlpha: 1 });
+  }
+
+  function hideInfo() {
+    let selected = document.querySelector(`button[slot="${this.slot}"] > div`);
+    gsap.to(selected, 1, { autoAlpha: 0 });
+  }
+
+  //Event Listener
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mouseout", hideInfo);
+  });
+})();
